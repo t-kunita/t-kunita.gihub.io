9ece48c65c8ccd3df100c391768381be5b842e66
t-kunita <138644278+t-kunita@users.noreply.github.com>
2023-08-31 07:19:48 +0900

    Create client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Data Table</title>
+</head>
+<body>
+  <table id="data-table">
+    <thead>
+      <tr>
+        <th>LocationID</th>
+        <th>Category</th>
+        <!-- <th>Age</th> -->
+      </tr>
+    </thead>
+    <tbody></tbody>
+  </table>
+  <script src="client.js"></script>
+</body>
+</html>
